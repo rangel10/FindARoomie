dfd6fc2408e60e2086d463aa1fbe1a7b12df27f8
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2018-10-27 17:11:26 -0500

    users.js

--- imports/api/rooms.js
+++ imports/api/rooms.js
@@ -1,1 +1,57 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+
+export const Rooms = new Mongo.Collection('rooms');
+
+if (Meteor.isServer) 
+{
+  Meteor.publish ('rooms', () => {
+    return Rooms.find({});
+  });
+}
+
+Meteor.methods(
+  {
+    'rooms.add': function() 
+    {
+      const userId = '' + (rooms.find({}).count+1);
+      const name = Meteor.user().username;
+
+      if(!name)
+      {
+        throw new Meteor.Error('Not Authorized');
+      }
+      rooms.upsert(
+        {userId},
+        {
+          userId: uId,
+          name,
+          win:0,
+          tie:0,
+          lost:0
+        });
+
+      const player = rooms.findOne({userId});
+      return player;
+    },
+
+    'rooms.findById': function(id){
+      const player = rooms.find({id: Meteor.userId()}).fetch();
+    },
+
+    'rooms.updateWin': function(id){
+      const player = rooms.find({id: Meteor.userId()}).fetch();
+      rooms.update(player[0].userId, win = win+1);
+    },
+
+    'rooms.updateLost': function(id){
+      const player = rooms.find({id: Meteor.userId()}).fetch();
+      rooms.update(player[0].userId, lost = lost+1);
+    },
+
+    'rooms.updateTie': function(id){
+      const player = rooms.find({id: Meteor.userId()}).fetch();
+      rooms.update(player[0].userId, tie = tie+1);
+    }
+  }
+);
