991dac3b73b51d6baa4f3f5e3b980b7bfb0fbcbe
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2018-10-27 16:08:13 -0500

    AccountsUpdate

---
+++ server/accounts-creation.js
@@ -0,0 +1,18 @@
+Accounts.onCreateUser(function(options, user) 
+{
+  // Use provided profile in options, or create an empty object
+  user.profile = options.profile || {};
+  // Assigns first and last names to the newly created user object
+  user.profile.firstName = options.firstName;
+  user.profile.lastName = options.lastName;
+  user.profile.profileImage = options.profileImage;
+  user.profile.email = options.email;
+  user.profile.phoneNumber = options.phoneNumber;
+  user.profile.profileFB = "";
+  user.profile.profileTW = "";
+  user.profile.type = options.type;
+  user.profile.rooms = "";
+  
+  // Returns the user object
+  return user;
+});
--- server/main.js
+++ server/main.js
@@ -1,4 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+// Imports de la carpeta Server
+import './account-creation.js';
+// Imports de la carpeta API Cliente
+// import  '../imports/api/players.js';
 
 Meteor.startup(() => {
 
