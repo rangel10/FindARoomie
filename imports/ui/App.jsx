771ae3efb769c86d761a710b6cc94280db3a18fa
Orlando Sabogal <o.sabogal10@uniandes.edu.co>
2018-10-27 15:42:44 -0500

    accounts

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
 
-const App = () => (
-  <div>
-    <h1>Welcome to FindARoomie!</h1>
-  </div>
-);
 
-export default App;
+//Imports carpeta ui
+import AccountsUIWrapper from './AccountsUIWrapper.js';
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div className = "app">
+            <AccountsUIWrapper/>
+      </div>
+    );
+  }
+}
+
+export default withTracker(() => {
+  return {
+    user: Meteor.user()
+  };
+})(App);
