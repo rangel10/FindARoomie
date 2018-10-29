00865a94b289f047e5047ee08ee162173e4824f8
Orlando Sabogal <o.sabogal10@uniandes.edu.co>
2018-10-29 00:02:12 -0500

    login ok

--- imports/ui/Login.jsx
+++ imports/ui/Login.jsx
@@ -5,54 +5,53 @@ import { withRouter } from 'react-router-dom';
 import '../styles/Login.css';
 
 export default class Login extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state={
-            email:'',
-            password:''
-        };
-        //Handlers
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      email:'',
+      password:''
+    };
+    //Handlers
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
     
-    handleSubmit(event)
-    {
-        event.preventDefault();
-        alert('User Loged In!');
-        
-    }
-    handleChange(event) 
-    {
-        let name = event.target.id;
-        let value = event.target.value;
-        this.setState({ [name]: value });
-    }
-    render() 
-    {
-        return(
-            <div class="form-box">
-            <div class="head">Find a Roomie</div>        
-            <form onSubmit={this.handleSubmit}
-            action="#" id="login-form">
-            <div class="form-group">
-            <label class="label-control">
-            <span class="label-text">Email</span>
-            </label>
-            <input type="email" name="email" class="form-control" onChange={this.handleChange} value={this.state.email} />
-            </div>
-            <div class="form-group">
-            <label class="label-control">
-            <span class="label-text">Password</span>
-            </label> 
-            <input type="password" name="password" class="form-control" onChange={this.handleChange} value={this.state.password}/>
-            </div>
-            <input type="submit" value="Login" class="btn" onClick={e=>this.onSubmit(e)}/>
-            <p class="text-p">Don't have an account? <a href="#">Sign up</a> </p>
-            </form>
-            </div>
+  handleSubmit(event)
+  {
+    event.preventDefault();
+    Meteor.loginWithPassword(this.state.email,this.state.password, function(err,result){
+      if(err){console.log(err);}
+      if(Meteor.user()){
+        window.location.assign('/viewRooms');
+      }
+    });
+    
+  }
+  handleChange(event) 
+  {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({ [name]: value });
+  }
+  render() 
+  {
+    return(
+      <div className="form-box">
+        <div className="head">Find a Roomie</div>        
+        <form onSubmit={this.handleSubmit}
+          action="#" id="login-form">
+          <div className="form-group">
+            <input placeholder='Email' type="email" name="email" className="form-control" onChange={this.handleChange} value={this.state.email} />
+          </div>
+          <div className="form-group">
+            <input placeholder='Password' type="password" name="password" className="form-control" onChange={this.handleChange} value={this.state.password}/>
+          </div>
+          <input type="submit" value="Login" className="btn" onClick={e=>this.handleSubmit(e)}/>
+          <p className="text-p">Don't have an account? <a href="/register">Sign up</a> </p>
+        </form>
+      </div>
             
-            );
-        }
-    }
+    );
+  }
+}
