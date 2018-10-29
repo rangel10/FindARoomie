9d7d1c386ea04d0e0b8f3f647546a1e67271e53d
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2018-10-29 01:24:06 -0500

    UserCardRevert
    mb por modificar esto..

--- imports/ui/UserCard.jsx
+++ imports/ui/UserCard.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {Meteor} from 'meteor/meteor';
-import {withTracker} from 'meteor/react-meteor-data';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,53 +8,65 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import {withRouter, Switch, Route, Link, Redirect} from 'react-router-dom';
 
-class UserCard extends Component {
-  
-  render(){
-    return (
-      <div>
-      <Card className="card">
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    // ⚠️ object-fit is not supported by IE 11.
+    objectFit: 'cover',
+  },
+};
+
+function ImgMediaCard(props) {
+  const { classes } = props;
+  let {
+    profileImage,
+    firstName,
+    lastName,
+    description,
+    type,
+    rooms
+  }=props;
+  return (
+    <Card className={classes.card}>
       <CardActionArea>
-      <CardMedia
-      component="img"
-      alt="UserInfo"
-      height="140"
-      weight="200"
-      image='https://static.thenounproject.com/png/17241-200.png'
-      title="UserInfo"
-      />
-      <CardContent>
-      <Typography gutterBottom variant="h5" component="h2">
-      User: {this.props.user.firstName} {this.props.user.lastName}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h5">
-      {this.props.user.type}
-      </Typography>
-      <Typography component="p">
-      {this.props.user.description}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h5">
-      {this.props.user.rooms}
-      </Typography>
-      
-      </CardContent>
+        <CardMedia
+          component="img"
+          alt="UserInfo"
+          className={classes.media}
+          height="140"
+          image={profileImage}
+          title="UserInfo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Owner: {firstName} {lastName}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h5">
+            {type}
+          </Typography>
+          <Typography component="p">
+            {description}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h5">
+           {rooms}
+          </Typography>
+          
+        </CardContent>
       </CardActionArea>
       <CardActions>
-      <Button size="small" color="primary">
-      Contact
-      </Button>
+        <Button size="small" color="primary">
+          Contact
+        </Button>
       </CardActions>
-      </Card>
-      </div>
-      );
-    }
-  }
-  
-  export default withTracker(() => {
-    Meteor.subscribe('users');
-    return {
-      user: Meteor.user()
-    };
-  })(UserCard);
+    </Card>
+  );
+}
+
+ImgMediaCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ImgMediaCard);
