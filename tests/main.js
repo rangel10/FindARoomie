8c57a44752a1682e26e6c0704bf6b86507d68c94
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-12-06 03:15:31 -0500

    Update main.js

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,6 @@
 import assert from "assert";
-
+// Code Review - Ricardo Andres Angel Villadiego
+// Hace falta preparar test
 describe("FindARoomie", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
