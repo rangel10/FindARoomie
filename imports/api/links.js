a19d815dbb1c4e75e0519fb908d0e3482e0f7db6
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-12-06 03:16:54 -0500

    Update links.js

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,3 +1,4 @@
 import { Mongo } from 'meteor/mongo';
-
+// Code Review - Ricardo Andres Angel Villadiego
+// Parece inecesaria esta colleccion
 export default Links = new Mongo.Collection('links');
